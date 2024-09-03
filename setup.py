b5b46b185641ac0e3702a8269b8a6950c0ff423f
Claudia Elliot-Wilson <c.elliotwilson@hotmail.com>
2024-09-03 09:24:56 +0200

    fixed errors

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name = 'calculator',
     version = '0.1', 
-    packages = find_packages(exclude['tests*']),
+    packages = find_packages(exclude=['tests*']),
     license='MIT',
     description = 'example calculator package', 
-    long_description = open(README.md).read(),
+    long_description = open('README.md').read(),
     install_requires = ['numpy'],
     url = 'url',
     author = 'Claudia', 
